335acfdc045b46e750b679e2c6e9872615f60380
JohnKihm <john.a.kihm@gmail.com>
2024-08-26 17:13:09 -0500

    created front end mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser() {
+        login() {
+
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser() {
+        addUser() {
+            
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook() {
+        saveBook() {
+            
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook() {
+        removeBook() {
+            
+        }
+    }
+`;
