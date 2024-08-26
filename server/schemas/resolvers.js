d15aa9f8753dcd40aabee4f53876da43f37a1bfe
JohnKihm <john.a.kihm@gmail.com>
2024-08-26 15:48:05 -0500

    created resolvers.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,23 @@
+const resolvers = {
+    Query: {
+        me: async () => {
+            return;
+        },
+    },
+    Mutation: {
+        login: async (parent, args) => {
+            return;
+        },
+        addUser: async (parent, args) => {
+            return;
+        },
+        saveBook: async (parent, args) => {
+            return;
+        },
+        removeBook: async (parent, args) => {
+            return;
+        },
+    },
+}
+
+module.exports = resolvers;
