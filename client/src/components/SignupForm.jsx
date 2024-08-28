eb6ae27f745ed4cabf55168234bae2b876864b52
JohnKihm <john.a.kihm@gmail.com>
2024-08-27 23:47:12 -0500

    cleaned up SignupForm.jsx

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
-
-// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
 const [createUser, { error }] = useMutation(ADD_USER);
@@ -31,17 +29,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser({
+      const { data } = await createUser({
         variables: { ...userFormData }
       });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
